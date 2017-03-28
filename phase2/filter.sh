e2f845e1e561838a7c3763b41ca985bfccf6b5f5
harleyvanselow <harleyvanselow@gmail.com>
2017-03-28 14:43:52 -0600

    Used provided perl script for final processing

--- phase2/filter.sh
+++ phase2/filter.sh
@@ -2,4 +2,6 @@
 sort "../phase1/tweets.txt" | uniq > filteredtweets
 sort "../phase1/terms.txt" | uniq > filteredterms
 sort "../phase1/dates.txt" | uniq > filtereddates
-
+$perl ./prepare.pl filteredtweets > finaltweets
+$perl ./prepare.pl filteredterms > finalterms
+$perl ./prepare.pl filtereddates > finaldates
