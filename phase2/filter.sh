6e2581a4a35cff11c59eb2d9cfd0a5afb0b190eb
Harley Vanselow <vanselow@cs.ualberta.ca>
2017-03-28 15:59:05 -0600

    Phase 2 working

--- phase2/filter.sh
+++ phase2/filter.sh
@@ -5,3 +5,6 @@ sort "../phase1/dates.txt" | uniq > filtereddates
 $perl ./prepare.pl filteredtweets > finaltweets
 $perl ./prepare.pl filteredterms > finalterms
 $perl ./prepare.pl filtereddates > finaldates
+db_load -f finaltweets -T -t hash tw.idx
+db_load -f finalterms -T -t btree te.idx
+db_load -f finaldates -T -t btree da.idx
