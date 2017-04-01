557d52ad886b611f1b46fbd66acd245b0961a498
Dannick Pomerleau <dannick@ualberta.ca>
2017-04-01 14:30:57 -0600

    Added 10k Sample

--- phase1/test_main.py
+++ phase1/test_main.py
@@ -2,11 +2,13 @@ from unittest import TestCase
 import xmlParse
 
 files = [['sample.xml',[('terms.txt', 'termscomp.txt'),('tweets.txt', 'tweetscomp.txt'),('dates.txt', 'datescomp.txt')]],
-         ['samplesbig.xml',[('terms.txt', 'termscompbig.txt'),('tweets.txt', 'tweetscompbig.txt'),('dates.txt','datescompbig.txt')]]]
+         ['samplesbig.xml',[('terms.txt', 'termscompbig.txt'),('tweets.txt', 'tweetscompbig.txt'),('dates.txt','datescompbig.txt')]],
+         ['sampleshuge.xml',[('terms.txt', 'termscomphuge.txt'),('tweets.txt', 'tweetscomphuge.txt'),('dates.txt','datescomphuge.txt')]]]
 
 
 class TestMain(TestCase):
     def test_main(self):
+        print("Running Main Test")
         for file in files:
             xmlParse.main(file[0])
             for check in file[1]:
